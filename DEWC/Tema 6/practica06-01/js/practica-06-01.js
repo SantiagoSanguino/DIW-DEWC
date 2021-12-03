a4b62cf4995765d6436427b6c8d42744f15ab953
SantiagoSanguino <91522514+SantiagoSanguino@users.noreply.github.com>
2021-12-03 10:41:26 +0100

    Add files via upload

--- DEWC/Tema 6/practica06-01/js/practica-06-01.js
+++ DEWC/Tema 6/practica06-01/js/practica-06-01.js
@@ -1,6 +1,31 @@
-window.onload=inicio;
+if (document.addEventListener)
+	window.addEventListener("load",inicio)
+else if (document.attachEvent)
+	window.attachEvent("onload",inicio);
 
-function inicio() {
-	// Al clickear
-	document.primerFormulario.addDefinicion.onclick=crearDefinicion;
+function inicio(){
+	var BtnMostrar=document.getElementById("registrarse");
+	var BtnOcultar=document.getElementById("ocultarR");
+	if (document.addEventListener) {
+		BtnMostrar.addEventListener("click",registrar);
+		BtnOcultar.addEventListener("click",ocultarR);
+	} else if (document.attachEvent) {
+		BtnMostrar.attachEvent("onclick",registrar);
+		BtnOcultar.attachEvent("onclick",ocultarR);
+	}
+	var BtnMostrar=document.getElementById("iniciarsesion");
+	var BtnOcultar=document.getElementById("ocultarE");
+	if (document.addEventListener) {
+		BtnMostrar.addEventListener("click",iniciosesion);
+		BtnOcultar.addEventListener("click",ocultarE);
+	} else if (document.attachEvent) {
+		BtnMostrar.attachEvent("onclick",iniciosesion);
+		BtnOcultar.attachEvent("onclick",ocultarE);
+	}
+	var boton=document.getElementById("addDefinicion");
+	if (document.addEventListener) {
+		boton.addEventListener("click",addDefin)
+	}else if (document.attachEvent) {
+		boton.attachEvent("onclick",addDefin);
+	}
 }
--- DEWC/Tema 6/practica06-01/js/validaciones.js
+++ DEWC/Tema 6/practica06-01/js/validaciones.js
@@ -1,20 +1,43 @@
 
-function crearDefinicion() {
-	let boton=document.getElementById("addDefinicion");
-	if (document.addEventListener)
-		boton.addEventListener("click",addDefinicion)
-	else if (document.attachEvent)
-		boton.attachEvent("onclick",addDefinicion);
+function registrar(){
+	document.getElementById("regist").setAttribute("open",true);
 }
-function addDefinicion() {
+function ocultarR(){
+	document.getElementById("regist").removeAttribute("open");
+}
+function iniciosesion(){
+	document.getElementById("entrar").setAttribute("open",true);
+}
+function ocultarE(){
+	document.getElementById("entrar").removeAttribute("open");
+}
+function addDefin() {
 	let padre=document.getElementById("definicion");
-	let vpalabra=document.getElementsByName("palabra").value.trim();
-	let newdt=document.createElement("dt");
-	let vconcepto=document.getElementsByName("concepto").value;
-	let newdd=document.createElement("dd");
-	let textopal=document.createTextNode(vpalabra);
-	let textocon=document.createTextNode(vconcepto);
+	let vpalabra=document.getElementById("palabra").value;
+	let vconcepto=document.getElementById("concepto").value;
 	if(vpalabra!=""&&vconcepto!="") {
-		
+		let alldt=padre.getElementsByTagName("dt");
+		let espalabra=true;		
+		let cont=0;
+		while (espalabra&&cont < alldt.length) {
+			if (alldt[cont].textContent==vpalabra) {
+				espalabra=false;
+			}
+			cont++;
+		}
+		let newdd=document.createElement("dd");
+		let textocon=document.createTextNode(vconcepto);
+		if(espalabra) {
+			let newdt=document.createElement("dt");
+			let textopal=document.createTextNode(vpalabra);
+			newdt.appendChild(textopal);
+			padre.appendChild(newdt);
+			newdd.appendChild(textocon);
+			padre.appendChild(newdd);
+		}else {
+			newdd.appendChild(textocon);
+			padre.insertBefore(newdd,alldt[cont]);
+		}
 	}
 }
+
