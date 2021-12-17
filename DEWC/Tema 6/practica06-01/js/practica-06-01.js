755899f396f0307ca20c8233f965005dc3283075
SantiagoSanguino <91522514+SantiagoSanguino@users.noreply.github.com>
2021-12-17 11:02:39 +0100

    Add files via upload

--- DEWC/Tema 6/practica06-01/js/practica-06-01.js
+++ DEWC/Tema 6/practica06-01/js/practica-06-01.js
@@ -1,9 +1,40 @@
+
 if (document.addEventListener)
 	window.addEventListener("load",inicio)
 else if (document.attachEvent)
 	window.attachEvent("onload",inicio);
 
 function inicio(){
+	var BtnMostrarR=document.getElementById("registrarse");
+	var BtnOcultarR=document.getElementById("ocultarR");
+	var BtnMostrarE=document.getElementById("iniciarsesion");
+	var BtnOcultarE=document.getElementById("ocultarE");
+	var BtnDefi=document.getElementById("addDefinicion");
+	var BtnLocal=document.getElementById("addLocalidad");
+	var BtnCoche=document.getElementById("addCoche");
+	var BtnComun=document.getElementById("comunidades");
+	if (document.addEventListener) {
+		BtnMostrarR.addEventListener("click",registrar);
+		BtnOcultarR.addEventListener("click",ocultarR);
+		BtnMostrarE.addEventListener("click",iniciosesion);
+		BtnOcultarE.addEventListener("click",ocultarE);
+		BtnDefi.addEventListener("click",addDefin);
+		BtnLocal.addEventListener("click",addLocal);
+		BtnCoche.addEventListener("click",addCoches);
+		BtnComun.addEventListener("change",mostrarComunidad);
+	} else if (document.attachEvent) {
+		BtnMostrarR.attachEvent("onclick",registrar);
+		BtnOcultarR.attachEvent("onclick",ocultarR);
+		BtnMostrarE.attachEvent("onclick",iniciosesion);
+		BtnOcultarE.attachEvent("onclick",ocultarE);
+		BtnDefi.attachEvent("onclick",addDefin);
+		BtnLocal.attachEvent("onclick",addLocal);
+		BtnCoche.attachEvent("onclick",addCoches);
+		BtnComun.attachEvent("onchange",mostrarComunidad);
+	}
+	
+	/*
+	//Registrarse
 	var BtnMostrar=document.getElementById("registrarse");
 	var BtnOcultar=document.getElementById("ocultarR");
 	if (document.addEventListener) {
@@ -13,6 +44,7 @@ function inicio(){
 		BtnMostrar.attachEvent("onclick",registrar);
 		BtnOcultar.attachEvent("onclick",ocultarR);
 	}
+	//Iniciar Sesion
 	var BtnMostrar=document.getElementById("iniciarsesion");
 	var BtnOcultar=document.getElementById("ocultarE");
 	if (document.addEventListener) {
@@ -22,10 +54,32 @@ function inicio(){
 		BtnMostrar.attachEvent("onclick",iniciosesion);
 		BtnOcultar.attachEvent("onclick",ocultarE);
 	}
+	//Añadir Definicion
 	var boton=document.getElementById("addDefinicion");
 	if (document.addEventListener) {
 		boton.addEventListener("click",addDefin)
 	}else if (document.attachEvent) {
 		boton.attachEvent("onclick",addDefin);
 	}
+	//Añadir Localidad
+	var boton=document.getElementById("addLocalidad");
+	if (document.addEventListener) {
+		boton.addEventListener("click",addLocal)
+	}else if (document.attachEvent) {
+		boton.attachEvent("onclick",addLocal);
+	}
+	//Añadir Coche
+	var boton=document.getElementById("addCoche");
+	if (document.addEventListener) {
+		boton.addEventListener("click",addCoches)
+	}else if (document.attachEvent) {
+		boton.attachEvent("onclick",addCoches);
+	}
+	//Seleccionar comunidad
+	var boton=document.getElementById("comunidades");
+	if (document.addEventListener) {
+		boton.addEventListener("change",mostrarComunidad)
+	}else if (document.attachEvent) {
+		boton.attachEvent("onchange",mostrarComunidad);
+	}/**/
 }
--- DEWC/Tema 6/practica06-01/js/validaciones.js
+++ DEWC/Tema 6/practica06-01/js/validaciones.js
@@ -11,6 +11,31 @@ function iniciosesion(){
 function ocultarE(){
 	document.getElementById("entrar").removeAttribute("open");
 }
+function comprobarUser(nombre) {
+	let sizenombre=nombre.length;
+	let esuser=true;
+	if(sizenombre>=8&&sizenombre<=12) {
+		let i=0;
+		let letras="áéíóúñ";
+		while(i<sizenombre) {
+			caracter=nombre[i].toLowerCase();
+			alert(caracter);
+			if(i<3) {
+				if((caracter<"a"||caracter>"z")&&!caracter.includes(letras)) {
+					esuser=false;
+				}
+			}else if(i>=3) {
+				if((caracter<"0"||caracter>"9")&&(caracter<"a"||caracter>"z")&&!caracter.includes(letras)) {
+					esuser=false;
+				}
+			}
+			i++;
+		}
+	}else {
+		esuser=false;
+	}
+}
+
 function addDefin() {
 	let padre=document.getElementById("definicion");
 	let vpalabra=document.getElementById("palabra").value;
@@ -40,4 +65,114 @@ function addDefin() {
 		}
 	}
 }
+function addLocal() {
+	let padre=document.getElementById("localidades");
+	let vlocal=document.getElementById("localidad").value.trim();
+	if(vlocal!="") {
+		let allli=padre.getElementsByTagName("li");
+		let eslocal=true;		
+		let cont=0;
+		while (eslocal&&cont < allli.length) {
+			let lin=allli.item(cont);
+			if (lin.textContent==vlocal) {
+				eslocal=false;
+			}else if (lin.textContent>vlocal) {
+				eslocal=false;
+				let newli=document.createElement("li");
+				let textolocal=document.createTextNode(vlocal);
+				newli.appendChild(textolocal);
+				lin.before(newli);
+			}
+			cont++;
+		}
+		if(eslocal) {
+			let newli=document.createElement("li");
+			let textolocal=document.createTextNode(vlocal);
+			newli.appendChild(textolocal);
+			padre.appendChild(newli);
+		}
+	}
+}
+function addCoches() {
+	let padre=document.querySelector("#coche>tbody");
+	let vmarca=document.getElementById("marca").value.trim();
+	let vmodelo=document.getElementById("modelo").value.trim();
+	let vprecio=document.getElementById("precio").value.trim();
+	if(vmarca!=""&&vmodelo!=""&&vprecio!="") {
+		let line=padre.getElementsByTagName("tr");
+		let escoche=true;		
+		let cont=0;
+		while (escoche&&cont < line.length) {
+			let lin=line.item(cont);
+			let celdas=lin.getElementsByTagName("td");
+			if (celdas.item(0).textContent==vmarca&&celdas.item(1).textContent==vmodelo) {
+				escoche=false;
+				celdas.item(2).textContent=vprecio;
+			}else if(celdas.item(0).textContent==vmarca&&celdas.item(1).textContent<vmodelo) {
+				escoche=false;
+				let newtr=document.createElement("tr");
+				let newmarca=document.createElement("td");
+				let newprecio=document.createElement("td");
+				let textmarca=document.createTextNode(vmarca);
+				let textprecio=document.createTextNode(vprecio);
+				newmarca.append(textmarca);
+				newmodelo.append(textmodelo);
+				newprecio.append(textprecio);
+				newtr.append(newmarca);
+				newtr.append(newmodelo);
+				newtr.append(newprecio);
+				lin.before(newtr);
+			}else if (celdas.item(0).textContent<vmarca) {
+				escoche=false;
+				let newtr=document.createElement("tr");
+				let newmarca=document.createElement("td");
+				let newprecio=document.createElement("td");
+				let textmarca=document.createTextNode(vmarca);
+				let textprecio=document.createTextNode(vprecio);
+				newmarca.append(textmarca);
+				newmodelo.append(textmodelo);
+				newprecio.append(textprecio);
+				newtr.append(newmarca);
+				newtr.append(newmodelo);
+				newtr.append(newprecio);
+				lin.before(newtr);
+			}
+			cont++;
+		}
+		if(escoche) {
+			let newtr=document.createElement("tr");
+			let newmarca=document.createElement("td");
+			let newmodelo=document.createElement("td");
+			let newprecio=document.createElement("td");
+			let textmarca=document.createTextNode(vmarca);
+			let textmodelo=document.createTextNode(vmodelo);
+			let textprecio=document.createTextNode(vprecio);
+			newmarca.append(textmarca);
+			newmodelo.append(textmodelo);
+			newprecio.append(textprecio);
+			newtr.append(newmarca);
+			newtr.append(newmodelo);
+			newtr.append(newprecio);
+			padre.append(newtr);
+		}
+	}
+}
 
+function addLocal() {
+	let provincias=document.getElementById("comunidades").value;
+	
+	if(provincias.length==0) {
+		return false;
+	}else {
+		document.getElementById("provincia").value()="Prueba1";
+		let newoption=document.createElement("option");
+		let textprovi=document.createTextNode("Prueba provincia");
+		newmarca.append(textmarca);
+		newmodelo.append(textmodelo);
+		newprecio.append(textprecio);
+		newtr.append(newmarca);
+		newtr.append(newmodelo);
+		newtr.append(newprecio);
+		padre.append(newtr);
+	}
+}
